3b6e5edf27a6db9f48bf663a948e1baf2d6b5689
Jack Harper <jack.harper@stfc.ac.uk>
2020-10-07 15:40:42 +0100

    adding jar build in gradle

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # combine-runinfo
 
-Kafka stream processor to combine output of instrument runInfo topics to an ALL_runInfo topic
+Kafka stream processor to combine output of several instrument `_runInfo` topics to an `ALL_runInfo` topic
 
 ### Usage
 
 ```
-gradle build
-./gradlew run
+gradle clean build
 ```
+
+Will generate a `.jar` file in `build/libs/` which you can run with `java -jar [filename]`
+
+Before this you should point the broker to whichever broker you want the processor to apply to. 
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,6 @@
-package main.kotlin.dev.rerpha.kafka.combine
+package main.kotlin.combine
 
 import mu.KotlinLogging
-
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.KafkaStreams
 import org.apache.kafka.streams.StreamsBuilder
@@ -18,39 +17,41 @@ fun main() {
     props[StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG] = Serdes.String().javaClass
 
     val builder = StreamsBuilder()
-    val instruments = setOf("LARMOR",
-            "ALF",
-            "DEMO",
-            "IMAT",
-            "MUONFE",
-            "ZOOM",
-            "IRIS",
-            "IRIS_SETUP",
-            "ENGINX_SETUP",
-            "HRPD",
-            "POLARIS",
-            "VESUVIO",
-            "ENGINX",
-            "MERLIN",
-            "RIKENFE",
-            "SELAB",
-            "EMMA-A",
-            "SANDALS",
-            "GEM",
-            "MAPS",
-            "OSIRIS",
-            "INES",
-            "TOSCA",
-            "LOQ",
-            "LET",
-            "MARI",
-            "CRISP",
-            "SOFTMAT",
-            "SURF",
-            "NIMROD",
-            "DETMON",
-            "EMU")
-    for (item in instruments){
+    val instruments = setOf(
+        "LARMOR",
+        "ALF",
+        "DEMO",
+        "IMAT",
+        "MUONFE",
+        "ZOOM",
+        "IRIS",
+        "IRIS_SETUP",
+        "ENGINX_SETUP",
+        "HRPD",
+        "POLARIS",
+        "VESUVIO",
+        "ENGINX",
+        "MERLIN",
+        "RIKENFE",
+        "SELAB",
+        "EMMA-A",
+        "SANDALS",
+        "GEM",
+        "MAPS",
+        "OSIRIS",
+        "INES",
+        "TOSCA",
+        "LOQ",
+        "LET",
+        "MARI",
+        "CRISP",
+        "SOFTMAT",
+        "SURF",
+        "NIMROD",
+        "DETMON",
+        "EMU"
+    )
+    for (item in instruments) {
         val topicName = item + "_runInfo"
         val inStream = builder.stream<String, String>(topicName)
         inStream.to("ALL_runInfo")
